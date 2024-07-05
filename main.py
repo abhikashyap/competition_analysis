f83da310b10db5759330c34dc323f79adfa889a4
abhikashyap <abhikshyap.ds@gmail.com>
2024-07-05 07:27:01 +0530

    changed scrapper request mode

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ with st.container():
             overall_progress.progress((i + 1) / len(list_of_keywords), text=f"{progress_text} ({i + 1}/{len(list_of_keywords)})")
             st.session_state['all_fsns'] = all_fsns
             driver=sc.open_chrome_headless()
-            fsns=sc.fns_scrap(driver,url,3)
+            fsns=sc.fns_scrap(driver,url,1)
             # driver.quit()
             all_fsns.extend(fsns)
         overall_progress.progress(1.0, text="Scraping complete!")
