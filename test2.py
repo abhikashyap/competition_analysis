391984df91cefd8c69869cae654ce15e4bde26ca
abhikashyap <abhikshyap.ds@gmail.com>
2024-07-05 13:38:38 +0530

    new scrapper

--- test2.py
+++ test2.py
@@ -1,8 +1,9 @@
 import test
+from scrapper import flipkart_json_scrapper_with_all_specifications as scrap
 import streamlit as st
 # a,response_code,response_header=test.scrap('ACCGZRZ2SSCF6Y4Y')
 # st.write(a)
 # st.write(response_code)
 # st.write(response_header)
-title=test.title_scrap('ACCGZRZ2SSCF6Y4Y')
-print(title)
+title=scrap.scrape_all_fsns(['ACCGZRZ2SSCF6Y4Y'])
+st.write(title)
